61e35d649a67c1abef0b0a2359311c48da3d1536
porko-app <ilieva17494@gmail.com>
2025-04-27 13:53:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -648,3 +648,14 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA2aVdbddoZDY7L_AlLWIwJDMuHxmh0HHk",
+    authDomain: "porko-bdad4.firebaseapp.com",
+    databaseURL: "https://porko-bdad4-default-rtdb.europe-west1.firebasedatabase.app", // Your database URL
+    projectId: "porko-bdad4",
+    storageBucket: "porko-bdad4.appspot.com",
+    messagingSenderId: "903004439010",
+    appId: "1:903004439010:web:33d37f46d52d1bc7d2b9bb",
+    measurementId: "G-PPTSVS7Z1R"
+};
