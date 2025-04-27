4cd070aa15469693acee3865602c2a8ca15079fe
porko-app <ilieva17494@gmail.com>
2025-04-27 21:14:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// Import Firebase modules at the top of the file
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
-import { getDatabase, ref, set, push, onValue, get } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyA2aVdbddoZDY7L_AlLWIwJDMuHxmh0HHk",
-    authDomain: "porko-bdad4.firebaseapp.com",
-    databaseURL: "https://porko-bdad4-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "porko-bdad4",
-    storageBucket: "porko-bdad4.firebasestorage.app",
-    messagingSenderId: "903004439010",
-    appId: "1:903004439010:web:33d37f46d52d1bc7d2b9bb",
-    measurementId: "G-PPTSVS7Z1R"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-
 document.addEventListener('DOMContentLoaded', () => {
     let selectedDrink = ""; // Global variable for selected drink
     let totalUnits = 0; // Global tracker for alcohol units
